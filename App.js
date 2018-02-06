12e427a9f6ba2022c0ba1d3aa9da15c7707f28bd
Lam Ngo <lam.ngo.qu@gmail.com>
2018-02-06 11:47:09 +0200

    Install react-navigation, add TabNavigator to entry component, separate CounterApp to a separate module in folder ./scenes

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import HomeScreen from './scenes/HomeScreen';
+import SettingScreen from './scenes/SettingScreen';
+import CounterApp from './scenes/CounterApp';
 
-export default class App extends React.Component {
-  state = { value: 0 };
-
-  increment = () => this.setState({ value: this.state.value + 1 });
-
-  decrement = () => this.setState({ value: this.state.value - 1 });
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Example counter app</Text>
-        <Text>Current value: {this.state.value}</Text>
-        <Button title="+" onPress={this.increment} />
-        <Button title="-" onPress={this.decrement} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default TabNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Setting: { screen: SettingScreen },
+    'Counter App': { screen: CounterApp },
   },
-});
+  {
+    tabBarPosition: 'bottom',
+  }
+);
