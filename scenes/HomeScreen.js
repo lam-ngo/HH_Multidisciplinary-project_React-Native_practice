f17964f16876eadaef9f86164df4e56d32faf5ed
Lam Ngo <lam.ngo.qu@gmail.com>
2018-02-06 11:43:39 +0200

    Added HomeScreen view component

---
+++ scenes/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
